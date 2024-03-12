9156cb87e7de4b945a67e13094a0a47957ff83da
Georg Thiesen <g.thiesen@posteo.de>
2024-03-12 12:29:10 +0100

    Configured workflows and CMakeLists.txt (#32)
    * ...
    
    * Update cmake-multi-platform-make-release.yml
    
    * Update cmake-multi-platform-make-release.yml
    
    * Update cmake-multi-platform-make-release.yml
    
    * Update cmake-multi-platform-make-release.yml
    
    * Update cmake-multi-platform-make-release.yml
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update cmake-multi-platform-make-release.yml
    
    * Update cmake-multi-platform.yml
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Ipagaxi patch 2 (#31)
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Now resources (images, sounds etc.) are copied to project_build during installation
    
    * ...
    
    * renamed workflows

--- src/include/Defines.hpp
+++ src/include/Defines.hpp
@@ -1,7 +1,7 @@
 #ifndef DEFINES_HPP
 #define DEFINES_HPP
 
-#define RESOURCE_PATH "../../src/resources/"
+#define RESOURCE_PATH "./resources/"
 #define GEN_IMG_WIDTH  512
 #define GEN_IMG_HEIGHT 512
 #define NUM_ENEMY 5
